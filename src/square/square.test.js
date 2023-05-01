9ae197fff08d1753ddc038102acb9682c3e383dc
VMirosh <miroshnychenko.vlad@gmail.com>
2023-05-01 15:15:30 +0200

    create test square, meet beforAll and afterAll

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -1,33 +1,43 @@
-const squareTest = require("./square");
- describe( 'squareValidation', ()=>{
-     let mockValue;
-     // Викликаеться функції перед кожним тестом
-     beforeEach(()=>{
-         // add base
+const square = require("./square");
+describe('squareValidation', () => {
 
-     })
-     // Викликаеться функції перед всима тестом
-     beforeAll(()=>{
 
-     })
+    let mockValue;
+    // Викликаеться  функції перед кожним тестом
+    beforeEach(() => {
+        // add base
+    })
+    // Викликаеться функції перед всима тестом
+    beforeAll(() => {
+    })
+    test('call 1', ()=>{
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(2)
+        expect(spyMathPow).toBeCalledTimes(1);
+    })
+    test('call 0 ', ()=>{
+        const spyMathPow = jest.spyOn(Math, 'pow');
+        square(1)
+        expect(spyMathPow).toBeCalledTimes(0);
+    })
 
-     test('square 2*2 ',()=>{
-         expect(squareTest(2)).toBe(4);
-     });
-     test('square a number > result  ',()=>{
-         expect(squareTest(2)).toBeGreaterThan(3);
-     });
-     test('square a number < result  ',()=>{
-         expect(squareTest(2)).toBeLessThan(5);
-     });
-     test('not undefined',()=>{
-         expect(squareTest(2)).not.toBeUndefined();
-     })
+    // test('square 2*2 ',()=>{
+    //     expect(squareTest(2)).toBe(4);
+    // });
+    // test('square a number > result  ',()=>{
+    //     expect(squareTest(2)).toBeGreaterThan(3);
+    // });
+    // test('square a number < result  ',()=>{
+    //     expect(squareTest(2)).toBeLessThan(5);
+    // });
+    // test('not undefined',()=>{
+    //     expect(squareTest(2)).not.toBeUndefined();
+    // })
+    afterAll(() => {
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-     afterAll(()=>{
-         //remove base
-         }
-     );
-     afterEach(()=>{});
 
- })
+})
